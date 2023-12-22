8464c21f9c5f789f4650ca9cafdb21583820fbf0
michaelc511 <michaelc511@gmail.com>
2023-12-22 11:34:46 -0800

    Minor things

--- src/drills/render/UserGreeting.jsx
+++ src/drills/render/UserGreeting.jsx
@@ -1,24 +1,11 @@
 
 function UserGreeting(props) {
 
-  // if (props.isLoggedIn) {
-  //   return (
-  //     <h2>Welcome {props.userName}</h2>
-  //   )
-  // }
-  // else {
-  //   return (
-  //     <h2>Please log in</h2>
-  //   )
-  // }
 
-  /*
-  Tenary 
-  */
-  return(props.isLoggedIn ? 
-    <h3>Welcome {props.userName} using Tenary</h3> : 
-    <h3>Please log in</h3>);
+  function UserGreeting(props) {
 
-}
+    return(<>Write code here</>)
+  
+  export default UserGreeting
 
 export default UserGreeting
