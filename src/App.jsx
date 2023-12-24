48bca1792e6d6e58e90a437684c56355dc90137f
michaelc511 <michaelc511@gmail.com>
2023-12-23 16:23:52 -0800

    Added more notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 // Drills
-  
+
 
 import './App.css'
 
@@ -10,7 +10,20 @@ function App() {
 
   return (
     <>
-      Please read README.md
+
+      Please read README.md for more details.
+
+      <ol>
+        <li>Props </li>
+        <li>Components  </li>
+        <li>Conditional Rendering  </li>
+        <li>Events</li>
+        <li>Looping through arrays</li>
+        <li>State management</li>
+        <li>Mini Project</li>
+      </ol>
+
+
     </>
   )
 }
