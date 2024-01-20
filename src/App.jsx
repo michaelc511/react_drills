617b2b9febf3afe8ddd10048da70b74f3bc36503
michaelc511 <michaelc511@gmail.com>
2024-01-20 14:23:03 -0800

    more updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,13 @@ import { useState } from 'react'
 
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { 
 
   return (
-    <>
-
-      Please read README.md for more details.
-
-      <ol>
-        <li>Props </li>
-        <li>Components  </li>
-        <li>Conditional Rendering  </li>
-        <li>Events</li>
-        <li>Looping through arrays</li>
-        <li>State management</li>
-        <li>Mini Project</li>
-      </ol>
-
-
-    </>
+    // create a div with a class of App
+    <div className="App">
+      <h1>React Drills</h1>
+    </div>
   )
 }
 
@@ -35,5 +22,5 @@ export default App
   1. Git:  
   2. Vid:  
   3. Blog:  
-  4. Solutions: in App.jsx or in README.md
+  4. Solutions:  
 */
